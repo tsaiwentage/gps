2de1ccca89344ddd1a90c67fcf6323fe7a7f5ba9
tsaiwentage <gzzxcaiwentian@163.com>
2019-11-14 17:40:34 +0800

    编译项目
    从老项目里把src拷贝过来

---
+++ src/gps_agent_pkg/catkin_generated/setup_cached.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/data/gps/src/gps_agent_pkg/devel:$CMAKE_PREFIX_PATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/data/gps/src/gps_agent_pkg/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/data/gps/src/gps_agent_pkg:/opt/ros/indigo/share:/opt/ros/indigo/stacks"
